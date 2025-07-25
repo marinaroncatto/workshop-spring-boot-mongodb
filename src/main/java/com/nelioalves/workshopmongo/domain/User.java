960eaf67865fe5112306f1a70579e11eba110f10
marinaroncatto <marinam.roncatto@gmail.com>
2025-07-25 09:42:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -3,9 +3,14 @@ package com.nelioalves.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user") //caso não indiquemos o nome, será mapeado segundo o nome da classe
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
