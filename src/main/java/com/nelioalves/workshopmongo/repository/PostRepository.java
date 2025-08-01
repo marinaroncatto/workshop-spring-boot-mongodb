ea5ee57caf28c20ed3dad80f9edad9d0f9fb376f
marinaroncatto <marinam.roncatto@gmail.com>
2025-08-01 09:11:06 -0300

    Consulta simples com @query

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,19 @@ package com.nelioalves.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//consulta personalizada utilizando linguagem do mongoDB
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	//busca no campo title do doc o primeiro parâmentro (?0) ingnorando case ('i')
+	
 	
 	//query method
 	List<Post> findByTitleContainingIgnoreCase(String text);
--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -22,8 +22,14 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	/*	
 	public List<Post> findByTitle(String text){
 		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	*/
+	
+	public List<Post> findByTitle(String text){
+		return repo.searchTitle(text);
 	}
 	
 }
