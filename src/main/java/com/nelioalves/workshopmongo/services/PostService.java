977446e0bb638bb8ceebee65103bb5925af1e49e
marinaroncatto <marinam.roncatto@gmail.com>
2025-08-05 09:50:31 -0300

    Consulta com varios criterios

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,6 +31,12 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000 );
+		//adiciona 24h ao dia limite para procurar em todos os horários até a meia noite.
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
